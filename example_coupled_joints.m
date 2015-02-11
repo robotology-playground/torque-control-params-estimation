52a2c1457538c277dc44310dcf44dae697ddb90d
Raffaello <raffaello.bonghi@officinerobotiche.it>
2015-02-11 09:31:21 +0100

    Clean up and add example to coupled joints

---
+++ example_coupled_joints.m
@@ -0,0 +1,20 @@
+%% Example to use this scripts
+% This file is usefull to understand the functionality about all object
+% included on this folder
+
+%% Load object Motor
+% Set all information about motor and robot when you would like to
+% experiments
+joint = CoupledJoints('iCubGenova03','torso');
+
+%% Load from file measure of friction
+joint = joint.loadIdleMeasure('idle');
+%joint = joint.setFrictionToCenter();
+%% Plot Friction
+%Counter figures
+if ~exist('counter','var')
+    counter = 1;
+end
+%% FIGURE - Friction data and estimation
+joint.savePictureFriction(counter);
+counter = counter + 1;
